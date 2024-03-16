cb8ab26fecce5af4e65ecb49e3edf9a9f2a963ad
Benjamin Minixhofer <bminixhofer@gmail.com>
2024-03-16 18:04:10 +0000

    fix maturin

--- bindings/python/tpu_build.sh
+++ bindings/python/tpu_build.sh
@@ -1,7 +1,7 @@
 # otherwise use 'maturin develop --release'
 set -e
 
-maturin build --release
+python3 -m maturin build --release
 mv $PWD/target/wheels/tokenizers-0.14.1.dev0-cp38-cp38-manylinux_2_31_x86_64.whl $PWD/target/wheels/tokenizers-0.14.1.dev0-cp38-none-any.whl
 pip install --force-reinstall $PWD/target/wheels/tokenizers-0.14.1.dev0-cp38-none-any.whl
 pip install fsspec==2023.9.2
